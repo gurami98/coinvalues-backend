ec7991b0f6774060c12894254986d66af5a47a4a
gurami-exceed <g.chilindrishvili.exceedteam@gmail.com>
2021-09-09 13:29:14 +0400

    add route for getting current coin

--- controllers/coinvaluesControllers.js
+++ controllers/coinvaluesControllers.js
@@ -17,4 +17,22 @@ const getCoins = async (req, res) => {
     }
 }
 
-module.exports = {getCoins}
+const getCurrentCoin = async (req, res) => {
+    try{
+        const response = await axios.get(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=${req.params.symbol}`, {
+            headers: {
+                'X-CMC_PRO_API_KEY': '6c5e97f3-9cdb-438f-8b01-77b71c8bd419',
+                'Accept': 'application/json',
+                'Accept-Encoding': 'deflate, gzip'
+            },
+            json: true,
+            deflate: true,
+            gzip: true
+        })
+        res.status(200).send(response.data)
+    }catch(e){
+        res.status(401).send('Error fetching data')
+    }
+}
+
+module.exports = {getCoins, getCurrentCoin}
--- routes/coinvaluesRoutes.js
+++ routes/coinvaluesRoutes.js
@@ -2,7 +2,8 @@ const express = require('express')
 const controllers = require('../controllers/coinvaluesControllers')
 const router = express.Router()
 
-const {getCoins} = controllers
+const {getCoins, getCurrentCoin} = controllers
 
 router.get('/get-all', getCoins)
+router.get('/get-coin/:symbol', getCurrentCoin)
 module.exports = router
