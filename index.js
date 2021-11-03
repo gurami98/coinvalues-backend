916863b59929f82b079c716e3af405e4b5d7cccb
gurami98 <chilindrishviligurami@gmail.com>
2021-11-04 03:40:08 +0400

    add port from env

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require('express')
 const app = express()
 const coin = require('./routes/coinvaluesRoutes')
 const cors = require('cors')
+const port = process.env.PORT || 3003
 
 app.use(express.json())
 app.use(cors())
 
 app.use('/coin', coin)
 
-app.listen(3003, () => console.log("Server Up and running"))
+app.listen(port, () => console.log("Server Up and running"))
 
 
 
